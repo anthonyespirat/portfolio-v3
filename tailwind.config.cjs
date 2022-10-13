930c9b0100d6b36e6248899d35f650d7b38f876a
anthonyespirat <jayshu666@gmail.com>
2022-10-13 22:55:30 +0200

    🎨 Init taiwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: [],
+  purge: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./public/index.html"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      "primary" : "#5688C7"
+    },
+    fontSize: {
+      "xs": ".75rem",
+      "sm": ".875rem",
+      "tiny": ".875rem",
+      "base": "1rem",
+      "lg": "1.125rem",
+      "xl": "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.75rem",
+      "4xl": "2rem",
+      "5xl": "2.5rem",
+      "3em" : "3em",
+      "6xl": "3.5rem",
+      "7xl": "4rem",
+      "8xl": "5rem",
+    },
+    borderRadius: {
+      DEFAULT : "10px"
+    },
+    container : {
+      padding : "1.5em",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
