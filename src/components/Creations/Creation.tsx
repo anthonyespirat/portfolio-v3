ee2557b70ab1164c14c205e4bfc2e5013c3efc6a
anthonyespirat <jayshu666@gmail.com>
2022-10-15 15:32:22 +0200

    🎨 Add MD

--- src/components/Creations/Creation.tsx
+++ src/components/Creations/Creation.tsx
@@ -1,3 +1,10 @@
-export default function Creations() {
-  return <div>Only one creation</div>;
+import type { PostFrontmatter } from "../../pages/index.astro";
+import { MarkdownInstance } from "astro";
+
+export default function Creation({
+  post,
+}: {
+  post: MarkdownInstance<PostFrontmatter>;
+}) {
+  return <div>{post.frontmatter.title}</div>;
 }
